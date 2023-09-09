2954d195135a3f80715a3d7ff220c7d0f57d5c76
Bruna Valadares <brunasfernandes94@outlook.com>
2023-09-09 20:25:39 -0300

    vai-de-promo-automacao

--- cypress.config.js
+++ cypress.config.js
@@ -5,8 +5,8 @@ module.exports = defineConfig({
     baseUrl: "https://www.vaidepromo.com.br",
     experimentalRunAllSpecs: true,
     specPattern: [
-      "cypress/e2e/passagensAerea.cy.js",
-      "cypress/e2e/seguroViagem.cy.js"
+      'cypress/e2e/passagensAerea.cy.js',
+      'cypress/e2e/seguroViagem.cy.js'
     ]
   },
 });
